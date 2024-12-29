a6396e2f84db9f0c07f8bc4db86eb3222c42e408
小蛋糕今天吃什么 <2927970764@qq.com>
2024-12-29 20:39:59 +0800

    1

---
+++ build/CMakeFiles/pca.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pca.dir/src/pca.cpp.obj"
+  "CMakeFiles/pca.dir/src/pca.cpp.obj.d"
+  "libpca.dll.a"
+  "pca.exe"
+  "pca.exe.manifest"
+  "pca.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pca.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
